31378848330d8b89fb96251d5dca0c241a06f6c5
HARSHAVARTHAN K G <102824883+HARSHAVARTHANKG@users.noreply.github.com>
2023-05-02 14:04:19 +0530

    Time: 5 ms (96.15%) | Memory: 43 MB (7.81%) - LeetSync

--- robot-return-to-origin/robot-return-to-origin.java
+++ robot-return-to-origin/robot-return-to-origin.java
@@ -6,25 +6,25 @@ class Solution {
 	    int u=0;
 	    int d=0;
 	 
-	    for(int i=0;i<moves.length();i++)
-	    {
-	        if(moves.charAt(i)=='U')
-	        {
-	            u++;
-	        }
-	        else if(moves.charAt(i)=='D')
-	        {
-	            d++;
-	        }
-	        else if(moves.charAt(i)=='L')
-	        {
-	            l++;
-	        }
-	        else
-	        {
-	           r++;
-	        }
-	    }
+	   for(char x : moves.toCharArray())
+		 {
+			 if(x=='R')
+			 {
+				 r++;
+			 }
+			 else if(x=='L')
+			 {
+				 l++;
+			 }
+			 else if(x=='U')
+			 {
+				 u++;
+			 }
+			 else
+			 {
+				 d++;
+			 }
+		 }
 	    if(l==r && u==d)
 	    {
 				return true;
