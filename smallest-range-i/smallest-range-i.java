941e6478b9670708e94e03271a903bd44aa531e4
HARSHAVARTHAN K G <102824883+HARSHAVARTHANKG@users.noreply.github.com>
2023-05-03 14:30:47 +0530

    Time: 5 ms (30.83%) | Memory: 43 MB (64.61%) - LeetSync

--- smallest-range-i/smallest-range-i.java
+++ smallest-range-i/smallest-range-i.java
@@ -2,13 +2,13 @@ class Solution {
     public int smallestRangeI(int[] nums, int k) 
     {
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
+        
         int low=nums[0]+k;
-        System.out.println(low);
+        
         int high=nums[nums.length-1]-k;
-        System.out.println(high);
+       
         int result=high-low;
-        System.out.println(result);
+    
 
         if(result<0)
         {
