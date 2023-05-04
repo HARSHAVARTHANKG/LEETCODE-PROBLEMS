0d582ba168f92e3418a7ec416dbad0333cf66011
HARSHAVARTHAN K G <102824883+HARSHAVARTHANKG@users.noreply.github.com>
2023-05-04 14:00:16 +0530

    Time: 4 ms (7.06%) | Memory: 41.1 MB (70.02%) - LeetSync

---
+++ remove-element/remove-element.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int removeElement(int[] nums, int val) 
+    {
+        	int count=0;
+		for(int i=0;i<nums.length;i++)
+		{
+		    if(nums[i]==val)
+		    {
+		        nums[i]=-1;
+		    }
+		    else
+		    {
+                count++;
+		        nums[i]=nums[i];
+		    }
+		}
+	    sort(nums);
+	    
+	    System.out.println(Arrays.toString(nums));
+
+        return count;
+		
+        
+    }
+
+    	public static void sort(int[] arr)
+	{
+	    for(int i=0;i<arr.length;i++)
+	    {
+	        for(int j=1;j<arr.length;j++)
+	        {
+	            if(arr[j-1]<arr[j])
+	            {
+	                int temp=arr[j-1];
+	                arr[j-1]=arr[j];
+	                arr[j]=temp;
+	            }
+	        }
+	    }
+	}
+}
