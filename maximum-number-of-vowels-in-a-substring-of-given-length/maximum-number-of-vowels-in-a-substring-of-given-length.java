16be6f22318128edb195b1a78a598123e05e08cf
HARSHAVARTHAN K G <102824883+HARSHAVARTHANKG@users.noreply.github.com>
2023-05-05 18:10:41 +0530

    Time: 41 ms (16.42%) | Memory: 43.6 MB (7.64%) - LeetSync

--- maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,26 +1,32 @@
 class Solution {
     public int maxVowels(String s, int k) 
+    {
+      int count=0;
+		
+		for(int i=0;i<k;i++)
 		{
-      	int n = s.length();
-  int[] arr = new int[n];
-  int count = 0;
-  for (int i = 0; i < n; i++) {
-      char c = s.charAt(i);
-      if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-          count++;
-      }
-      arr[i] = count;
-  }
-  int max = 0;
-  for (int i = 0; i <= n - k; i++) {
-      int end = i + k - 1;
-      if (i == 0) {
-          max = Math.max(max, arr[end]);
-      } else {
-          max = Math.max(max, arr[end] - arr[i - 1]);
-      }
-  }
-  return max;
-        
+		    if(("aeiouAEIOU").contains(s.valueOf(s.charAt(i))))
+		    {
+		        count++;
+		    }
+		}
+		
+		int max = count;
+		
+		for(int i=k;i<s.length();i++)
+		{
+		    if(("aeiouAEIOU").contains(s.valueOf(s.charAt(i-k))))
+		    {
+		        count--;
+		    }
+		    
+		    if(("aeiouAEIOU").contains(s.valueOf(s.charAt(i))))
+		    {
+		        count++;
+		    }
+		    
+		    max = Math.max(max,count);
+		}
+    return max;
     }
 }
