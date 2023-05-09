b1a3e6d5136ab1a4594bf6f75a1da744678e7739
HARSHAVARTHAN K G <102824883+HARSHAVARTHANKG@users.noreply.github.com>
2023-05-09 18:54:14 +0530

    Time: 1332 ms (32.77%) | Memory: 38.9 MB (98.44%) - LeetSync

--- JavaAndSql/perfect-number/perfect-number.java
+++ JavaAndSql/perfect-number/perfect-number.java
@@ -1,11 +1,38 @@
 class Solution {
     public boolean checkPerfectNumber(int num) 
     {
-       if(num==28 || num==6 || num==496 || num==8128 | num==33550336)
+      /*  
+      if(num==28 || num==6 || num==496 || num==8128 | num==33550336)
        {
            return true;
        }
        return false;
+       
+*/
+      //this Approach also will work
+
+        if(num%2!=0)
+        {
+            return false;
+        }
+        int sum=0;
+        for(int i=1;i<=num/2;i++)
+        {
+            if(num%i==0)
+            {
+                sum+=i;
+            }
+           
+        }
+        
+        if(sum==num)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
         
     }
 }
