163ae6167fd5df87b889fa7dde8a2ef1b781ecc0
HARSHAVARTHAN K G <102824883+HARSHAVARTHANKG@users.noreply.github.com>
2025-04-22 21:44:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (44.25%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string convertToString(int n)
+    {
+        string ret;
+        while(n!=0)
+        {
+            ret = (char)(n%2+'0') + ret;
+            n=n/2;
+        }
+        
+        for(int i=0;i<ret.length();i++)
+        {
+            if(ret[i]=='0')
+            {
+                ret[i]='1';
+            }
+            else
+            {
+                ret[i]='0';
+            }
+        }
+        return ret;
+    }
+
+    int convertToNumber(string str)
+    {
+        int ret=0;
+        long long power = 1;
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            int number = (int)(str[i]-'0');
+            ret = ret + number*power;
+            power = power*2;
+        }
+        return ret;
+    }
+    int findComplement(int num) 
+    {
+       
+         string str = convertToString(num);
+        int result = convertToNumber(str);
+        return result;
+        
+    }
+};
