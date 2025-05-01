1384ed62ea5a1faa96731182a1568aadb2abec41
HARSHAVARTHAN K G <102824883+HARSHAVARTHANKG@users.noreply.github.com>
2025-05-01 19:56:52 +0530

    Time: 3 ms (6.18%) | Memory: 8.2 MB (59.91%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void rotateIt(string &s)
+    {
+        char c = s[0];
+
+        for(int i=1;i<s.length();i++)
+        {
+            s[i-1]=s[i];
+        }
+        s[s.length()-1]=c;
+    }
+    bool rotateString(string s, string goal)
+    {
+        int i=0;
+
+        while(i!=s.length())
+        {
+            if(s==goal)
+            {
+                return true;
+            }
+            rotateIt(s);
+            cout<<s<<endl;
+            i++;
+        }
+        return false;
+    }
+};
