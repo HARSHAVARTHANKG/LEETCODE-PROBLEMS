a84033012e869df5b10e439d9de7c26563dc1e55
HARSHAVARTHAN K G <102824883+HARSHAVARTHANKG@users.noreply.github.com>
2026-02-23 21:46:22 +0530

    Time: 3 ms (19.67%) | Memory: 14.2 MB (28.95%) - LeetSync

---
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) 
+    {
+        int a[501]={0};
+
+        for(int i=0;i<arr.size();i++)
+        {
+            a[arr[i]]++;
+        }        
+
+        int max = -1;
+
+        for(int i = 0;i<=500;i++)
+        {
+           if(i == a[i] && a[i]!=0)
+            {
+                if(max < a[i])
+                {
+                    max = i;
+                }
+            }
+        }
+        return max;
+    }
+};
