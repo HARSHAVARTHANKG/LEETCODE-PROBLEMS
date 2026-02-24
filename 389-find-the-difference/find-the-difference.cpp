38e5cf114c4440467bf311c833bc37571006682e
HARSHAVARTHAN K G <102824883+HARSHAVARTHANKG@users.noreply.github.com>
2026-02-24 22:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (50.10%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -2,17 +2,31 @@ class Solution {
 public:
     char findTheDifference(string s, string t) 
     {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        int flag=0;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // int flag=0;
         
-        for(int i=0;i<t.length()-1;i++)
+        // for(int i=0;i<t.length()-1;i++)
+        // {
+        //     if(s[i]!=t[i])
+        //     {
+        //         return t[i];            
+        //     }
+        // }
+        // return t[t.length()-1];
+         int result=0;
+        
+        for(int x : s)
         {
-            if(s[i]!=t[i])
-            {
-                return t[i];            
-            }
+            result = result^x;
         }
-        return t[t.length()-1];
+        
+        
+        for(int x : t)
+        {
+            result = result^x;
+        }
+        
+        return result;
     }
 };
